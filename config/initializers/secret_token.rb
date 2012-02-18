5e20e44ce660410d9b197e9a3222ea6643a8d7c5
unknown <kalpit@ace-130fe3e36e9.(none)>
2012-02-18 12:44:51 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRMVer10::Application.config.secret_token = 'ee74956ee10a530ea59cb4342cb0d684c1cb2a2390f052a9f62f74b2171131d9bb8350daeb01b7f228f41ae582be68f7a7a26f67aafbebe9f46be13f33879046'
